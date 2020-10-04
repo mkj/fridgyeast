6a9b17a8ddd01ef01f828b01f8c1c0084ca28f50
Matt Johnston <matt@ucc.asn.au>
2020-10-04 23:27:06 +0800

    forgot the build script

---
+++ build.rs
@@ -0,0 +1,19 @@
+use std::process::Command;
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let revid = Command::new("hg").args(&["id", "-i"])
+    .output()
+    .map(|o| String::from_utf8(o.stdout).unwrap())
+    .unwrap_or("No hg revid".to_string());
+    
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("hg-revid.txt");
+    fs::write(
+        &dest_path,
+        revid).unwrap();
+    println!("cargo:rerun-if-changed=.hg");
+}
+
