8f1507c0dcc7df86c327beac09ab7937ff562945
Matt Johnston <matt@ucc.asn.au>
2021-02-13 00:37:28 +0800

    Use systemctl instead of tmux to deploy

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ set -x
 
 cargo build --release --target arm-unknown-linux-musleabihf
 arm-linux-gnueabi-strip -S target/arm-unknown-linux-musleabihf/release/fridgyeast
-cat target/arm-unknown-linux-musleabihf/release/fridgyeast | ssh ferment.local "mv -f fridgyeast fridgyeast.old ; cat > fridgyeast && chmod u+x fridgyeast && (tmux kill-session -t ferment; tmux new -d -s ferment ./fridgyeast; sleep 0.3; tail -20 fridgyeast.log)"
+cat target/arm-unknown-linux-musleabihf/release/fridgyeast | ssh ferment.local "mv -f fridgyeast fridgyeast.old ; cat > fridgyeast && chmod u+x fridgyeast && sudo systemctl restart fridgyeast; sleep 0.3; tail -20 fridgyeast.log"
